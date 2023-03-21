d1e7c3734bfe63700baae227679ec2656757c46f
Dilsher Balouch <dilsherb@gmail.com>
2023-03-21 19:24:27 +0300

    footer added.

---
+++ src/footer.js
@@ -0,0 +1,8 @@
+const footer = () => {
+  const ourFooter = `
+    <div class="footer"> Created by Rishi Mishra and Dilsher Balouch under Microverse guidance </div>
+  `;
+  return ourFooter;
+}
+
+export default footer;
