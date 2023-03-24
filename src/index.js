0bfb1e4a65bdea03c9b693239c42e694ad003400
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-03-24 13:38:25 +0530

    Make code modular

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import './index.css';
-import showPokemon from './modules/showPokemon.js';
-import navbarHandler from './modules/navbar.js';
-import showAbilities from './modules/abilities.js';
-import showStats from './modules/stats.js';
-
-navbarHandler();
-showPokemon();
-showAbilities();
-showStats();
+import './modules/getData.js';
+import './modules/renderData.js';
+import './modules/addLike.js';
